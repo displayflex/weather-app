54add554dcdb1293ea4968ef010f6b53381bb9ab
Vadzim Sliaptsou <vadim.sleptsov@innowise-group.com>
2019-09-10 14:07:43 +0300

    Move loadYandexScript to SetupPage

--- src/components/pages/Setup/component.jsx
+++ src/components/pages/Setup/component.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import StandardLayout from '@/components/layouts/Standard'
 import SetupPanel from '@/components/forms/SetupPanel'
+import Loader from '@/components/blocks/global/Loader'
+import { loadYandexScript } from '@/utils/services'
 
-const SetupPage = () => (
-  <StandardLayout>
-    <SetupPanel />
-  </StandardLayout>
-)
+class SetupPage extends Component {
+  state = {
+    isScriptLoading: true,
+    isScriptLoadedWithError: false,
+  }
+
+  componentDidMount () {
+    const scriptSuccesLoadHandler = () => {
+      this.setState({ isScriptLoading: false })
+    }
+
+    const scriptErrorLoadHandler = () => {
+      this.setState({ isScriptLoadedWithError: true })
+    }
+
+    loadYandexScript(scriptSuccesLoadHandler, scriptErrorLoadHandler)
+  }
+
+  render () {
+    if (this.state.isScriptLoadedWithError) {
+      return <p>Something went wrong</p> // @todo
+    }
+
+    return (
+      <StandardLayout>
+        {this.state.isScriptLoading ? <Loader /> : <SetupPanel />}
+      </StandardLayout>
+    )
+  }
+}
 
 export default SetupPage
