0086ec70ef9697cce999eafb9ef4972b1f70794b
Vadzim Sliaptsou <vadim.sleptsov@innowise-group.com>
2019-09-16 12:09:09 +0300

    Move services list from store to services.json

--- src/reducer/services.js
+++ src/reducer/services.js
@@ -1,24 +1,14 @@
 import { CHANGE_SERVICE } from '@/constants/actions'
-import { OPEN_WEATHER, WEATHERSTACK } from '@/constants/services'
+import allServices from '@/services.json'
 
 const initialState = {
-  all: [
-    {
-      id: OPEN_WEATHER,
-      name: 'Open Weather',
-    },
-    {
-      id: WEATHERSTACK,
-      name: 'Weatherstack',
-    },
-  ],
   current: 'OPEN_WEATHER',
 }
 
 const services = (state = initialState, action) => {
   switch (action.type) {
     case CHANGE_SERVICE:
-      if (initialState.all.filter(service => service.id === action.payload.id).length > 0) {
+      if (allServices.filter(service => service.id === action.payload.id).length > 0) {
         return {
           ...state,
           current: action.payload.id,
