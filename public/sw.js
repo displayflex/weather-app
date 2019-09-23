b0da1135721f96510f67dfd5c0e1994d90c33d7c
Vadzim Sliaptsou <vadim.sleptsov@innowise-group.com>
2019-09-23 19:06:04 +0300

    Implemented offline work using service workers

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,7 @@
-var CACHE_NAME = 'v1'
-var urlsToCache = ['/weather']
+const CACHE_NAME = 'v1'
+const urlsToCache = ['/', '/weather']
 
 self.addEventListener('install', function (event) {
-  console.log('[SW] install')
   event.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
       return cache.addAll(urlsToCache)
@@ -11,8 +10,7 @@ self.addEventListener('install', function (event) {
 })
 
 self.addEventListener('activate', function (event) {
-  console.log('[SW] activate')
-  var cacheWhitelist = ['v1']
+  const cacheWhitelist = [CACHE_NAME]
 
   event.waitUntil(
     caches.keys().then(function (cacheNames) {
@@ -27,45 +25,17 @@ self.addEventListener('activate', function (event) {
   )
 })
 
-// self.addEventListener('fetch', function (event) {
-//   console.log('[SW] fetch')
-//   event.respondWith(
-//     caches.match(event.request).then(function (response) {
-//       // Cache hit - return response
-//       if (response) {
-//         return response
-//       }
-//       return fetch(event.request)
-//     })
-//   )
-// })
-
 self.addEventListener('fetch', function (event) {
-  console.log('[SW] fetch')
   event.respondWith(
-    caches.match(event.request).then(function (response) {
-      // Cache hit - return response
-      if (response) {
-        return response
-      }
-
-      return fetch(event.request).then(function (response) {
-        // Check if we received a valid response
-        if (!response || response.status !== 200 || response.type !== 'basic') {
-          return response
-        }
-
-        // IMPORTANT: Clone the response. A response is a stream
-        // and because we want the browser to consume the response
-        // as well as the cache consuming the response, we need
-        // to clone it so we have two streams.
-        var responseToCache = response.clone()
-
-        caches.open(CACHE_NAME).then(function (cache) {
-          cache.put(event.request, responseToCache)
-        })
-
-        return response
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.match(event.request).then(function (response) {
+        return (
+          response ||
+          fetch(event.request).then(function (response) {
+            cache.put(event.request, response.clone())
+            return response
+          })
+        )
       })
     })
   )
--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -19,7 +19,7 @@ const isLocalhost = Boolean(
 )
 
 export function register (config) {
-  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+  if ('serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href)
     if (publicUrl.origin !== window.location.origin) {
@@ -31,7 +31,6 @@ export function register (config) {
 
     window.addEventListener('load', () => {
       const swUrl = `${process.env.PUBLIC_URL}/sw.js`
-
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
         checkValidServiceWorker(swUrl, config)
